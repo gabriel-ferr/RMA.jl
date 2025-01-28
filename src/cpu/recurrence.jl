fff40bc70eba191e5bce11b3a42452317e89aa9d
gabriel-ferr <gabriel.vferreira@icloud.com>
2025-01-28 15:07:27 -0300

    Update 0.0.3-DEV: Add the corridor threshold recurrence function

--- src/cpu/recurrence.jl
+++ src/cpu/recurrence.jl
@@ -3,3 +3,8 @@
 function recurrence(x::AbstractVector, y::AbstractVector, threshold::Union{Float32, Float64})
     return (threshold - euclidean(x, y)) >= 0 ? Int8(1) : Int8(0)
 end
+#
+#           Corridor Recurrence
+function recurrence(x::AbstractVector, y::AbstractVector, threshold::Tuple{Union{Float32, Float64}, Union{Float32, Float64}})
+    return ((euclidean(x, y) - threshold[1]) >= 0 ? Int8(1) : Int8(0)) * (threshold[2] - euclidean(x, y)) >= 0 ? Int8(1) : Int8(0)
+end
